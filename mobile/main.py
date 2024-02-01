30e668942680cb78ab239dc452de49b431760c3f
mpsloan <sloan.40@wright.edu>
2024-02-01 09:09:54 -0500

    handles post request

--- mobile/main.py
+++ mobile/main.py
@@ -1,6 +1,10 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, url_for, flash, redirect
+import os
 
+print(os.urandom(24).hex())
 app = Flask(__name__)
+
+app.config['SECRET_KEY'] = '6cddf7756b9c57390fb7630019a7da4bb1ac822ccbcbaba1'
 
 messages = [{'title': 'Message One',
              'content': 'Message One Content'},
@@ -15,6 +19,18 @@ def index():
 
 @app.route('/create/', methods=('GET', 'POST'))
 def create():
+    if request.method == 'POST':
+        title = request.form['title']
+        content = request.form['content']
+
+        if not title:
+            flash('Title is required!')
+        elif not content:
+            flash('Content is required!')
+        else:
+            messages.append({'title': title, 'content': content})
+            return redirect(url_for('index'))
+
     return render_template('create.html')
 
 # make sure this is the last line in file
