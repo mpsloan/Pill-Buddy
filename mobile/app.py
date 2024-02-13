75e32d2d869927b2bc999b5efad414ee803c0757
mpsloan <sloan.40@wright.edu>
2024-02-13 09:37:07 -0500

    empty text field handling

--- mobile/app.py
+++ mobile/app.py
@@ -23,19 +23,45 @@ def index():
 def create():
 
     if request.method == 'POST':
-        1name = request.form['1name']
-        1dispense = request.form['1dispense']
-        1times = request.form['1times']
-        1days = request.form['1days']
+        name1 = request.form['1name']
+        dispense1 = request.form['1dispense']
+        times1 = request.form['1times']
+        days1 = request.form['1days']
 
-        if not 1name:
-            flash('Name is required!')
-        elif not 1dispense:
-            flash('Dispense is required!')
-        elif not 1times:
-            flash('Times is required!')
-        elif not 1days:
-            flash('Days is required!')
+        name2 = request.form['2name']
+        dispense2 = request.form['2dispense']
+        times2 = request.form['2times']
+        days2 = request.form['2days']
+
+        name3 = request.form['3name']
+        dispense3 = request.form['3dispense']
+        times3 = request.form['3times']
+        days3 = request.form['3days']
+
+        if not name1:
+            flash("Pill 1's name is required!")
+        elif not dispense1:
+            flash("Pill 1's dispense is required!")
+        elif not times1:
+            flash("Pill 1's times are required!")
+        elif not days1:
+            flash("Pill 1's days are required!")
+        elif not name2:
+            flash("Pill 2's name is required!")
+        elif not dispense2:
+            flash("Pill 2's dispense is required!")
+        elif not times2:
+            flash("Pill 2's times are required!")
+        elif not days2:
+            flash("Pill 2's days are required!")
+        elif not name3:
+            flash("Pill 3's name is required!")
+        elif not dispense3:
+            flash("Pill 3's dispense is required!")
+        elif not times3:
+            flash("Pill 3's times are required!")
+        elif not days3:
+            flash("Pill 3's days are required!")
         else:
             # messages.append({'title': title, 'content': content})
             return redirect(url_for('index'))
@@ -43,7 +69,4 @@ def create():
     return render_template('create.html')
 
 # make sure this is the last line in file
-
-
-# git add/commit
 app.run()
