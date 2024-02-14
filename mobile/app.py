b5218be70353992891fb984f3388bafc1f88ada2
mpsloan <sloan.40@wright.edu>
2024-02-13 19:21:15 -0500

    create list to hold form entries

--- mobile/app.py
+++ mobile/app.py
@@ -25,23 +25,46 @@ def create():
 
     if request.method == 'POST':
 
+        form_list = []
+
         # pill 1's fields
         name1 = request.form['1name']
+        form_list.append(name1)
+
         dispense1 = request.form['1dispense']
+        form_list.append(dispense1)
+
         times1 = request.form['1times']
+        form_list.append(times1)
+
         days1 = request.form['1days']
+        form_list.append(days1)
 
         # pill 2's fields
         name2 = request.form['2name']
+        form_list.append(name2)
+
         dispense2 = request.form['2dispense']
+        form_list.append(dispense2)
+
         times2 = request.form['2times']
+        form_list.append(times2)
+
         days2 = request.form['2days']
+        form_list.append(days2)
 
         # pill 3's fields
         name3 = request.form['3name']
+        form_list.append(name3)
+
         dispense3 = request.form['3dispense']
+        form_list.append(dispense3)
+
         times3 = request.form['3times']
+        form_list.append(times3)
+
         days3 = request.form['3days']
+        form_list.append(days3)
 
         # making sure each field on form contains information
         if not name1:
@@ -72,7 +95,24 @@ def create():
             # messages.append({'title': title, 'content': content})
             return redirect(url_for('index'))
 
+        # user_input = request.form['pill_data']
+        # with open('test.txt', 'w') as f:
+        #     f.write(str(user_input))
+
+        f = open('test.txt', 'a')
+        with open('test.txt', 'w') as f:
+            f.write(str(form_list))
+            # for entry in form_list:
+            #     f.write(entry)
+
     return render_template('create.html')
+
+# @app.route('/', methods=['POST'])
+# def write_data():
+#     user_input = request.form['pill_data']
+#     with open('test.txt', 'w') as f:
+#         f.write(str(user_input))
+#     return ('create.html')
 
 # make sure this is the last line in file
 app.run()
