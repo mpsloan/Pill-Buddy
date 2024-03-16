751e1e2014f5f6a86617a002f21cf4f81f75ea95
mpsloan <sloan.40@wright.edu>
2024-03-16 17:26:42 -0400

    apply mask and display color

--- OpenCV/detection.py
+++ OpenCV/detection.py
@@ -10,4 +10,10 @@ lower_range = (0, 50, 50)
 upper_range = (10, 255, 255)
 mask = cv2.inRange(hsv_img, lower_range, upper_range)
 
-color_image = cv2.bitwise_and(image, image, mask=mask)
+# Apply mask to image
+color_image = cv2.bitwise_and(img, img, mask=mask)
+
+# Display color image
+cv2.imshow('Color Image', color_image)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
