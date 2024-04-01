75b921a9b0785521b2aa8e2630ad6247ddd718b4
mpsloan <sloan.40@wright.edu>
2024-04-01 18:05:39 -0400

    prettier format

--- OpenCV/detection.py
+++ OpenCV/detection.py
@@ -2,20 +2,23 @@
 # https://stackoverflow.com/questions/10948589/choosing-the-correct-upper-and-lower-hsv-boundaries-for-color-detection-withcv
 import cv2
 import numpy as np
+
 # load image
 img = cv2.imread("../images/two.jpg")
+
 # Convert to HSV
 hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
 
 # BLUE
-# define range wanted color in HSV
+# define HSV color range
 lower_val = np.array([90,100,20])
 upper_val = np.array([130,255,255])
 
-# Threshold the HSV image - any green color will show up as white
+# Threshold the HSV image - any specified color will show up as white
 mask = cv2.inRange(hsv, lower_val, upper_val)
 
-# if there are any white pixels on mask, sum will be > 0
+# check against large sum of white pixels
+# because there are small amounts of most colors on most pictures
 hasBlue = np.sum(mask)
 if hasBlue > 10000000:
     print('Blue detected!')
@@ -50,6 +53,7 @@ else:
 # apply mask to image
 res = cv2.bitwise_and(img,img,mask=mask)
 fin = np.hstack((img,res))
+
 # display image
 cv2.imshow("Res", fin)
 cv2.imshow("Mask", mask)
