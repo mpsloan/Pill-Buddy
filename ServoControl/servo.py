e59ace704823059176e383c0cdffc20c49659023
mpsloan <sloan.40@wright.edu>
2024-04-04 08:45:03 -0400

    add new link

--- ServoControl/servo.py
+++ ServoControl/servo.py
@@ -1,4 +1,5 @@
 #Example from https://gpiozero.readthedocs.io/en/stable/api_output.html#servo
+# https://www.digikey.com/en/maker/tutorials/2021/how-to-control-servo-motors-with-a-raspberry-pi#:~:text=This%20simple%20python%20script%20first,waits%20for%20half%20a%20second
 from gpiozero import Servo
 from time import sleep
 
